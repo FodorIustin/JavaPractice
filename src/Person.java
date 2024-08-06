1fa4d498e4358bb30fa2efa716c970e228dd68a6
Iustin Fodor <iustin.fodor@globallogic.com>
2024-08-06 01:54:56 -0700

    constructor getters and setters

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private int age;
+    private String name;
+    private double height;
+
+    public Person(int age, String name, double height) {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+    }
+
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
